64326aca5e528dac44a07530cbf965d363b21d3d
FeliciaFredlund <felicia.fredlund@gmail.com>
2024-10-31 16:18:06 +0100

    Player class added and implemented

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Always make sure the virtual environment was running while starting the game or 
 
 Use this command to start it:
 source venv/bin/activate
+
+## The way I figured out how to do it vs what was simplier
+1. 
+How I did it:
+`pygame.Surface.fill(screen, (0, 0, 0))`
+
+How it is easier to do a black screen with pygame:
+`screen.fill("black")`
--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+
+        # Updating objects 
+        for obj in updateable:
+            obj.update(dt)
+
+        # Drawing
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-    
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
